a51274b4702ed910f7beeebccb08402cdaee855d
Benjamin Byun <benjaminbyun@gmail.com>
2020-08-13 14:23:48 -0400

    Changed nav bar

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded',function(event){
   // array with texts to type in typewriter
-  var dataText = [ "Nice to meet you",];
+  var dataText = [ "Welcome to my page.",];
 
   // type one text in the typwriter
   // keeps calling itself until the text is finished
